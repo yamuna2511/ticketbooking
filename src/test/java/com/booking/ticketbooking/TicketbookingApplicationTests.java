d3fa2d6014b623474d415378b7d71741b59b9b83
yamuna2511 <yamuna2511@gmail.com>
2026-09-05 20:01:07 +0200

    junit changes

--- src/test/java/com/booking/ticketbooking/TicketbookingApplicationTests.java
+++ src/test/java/com/booking/ticketbooking/TicketbookingApplicationTests.java
@@ -1,13 +1,16 @@
 package com.booking.ticketbooking;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
 class TicketbookingApplicationTests {
 
-	@Test void applicationTest() { assertTrue(true); }
+
+	@Test
+	void applicationTest() {
+		assertTrue(true);
+	}
+
 
 }
